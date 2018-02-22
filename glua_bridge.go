4b7e44fc130350c5f719496ad6e51c48019b5b87
zhangliang <zhangliang.ryou@bytedance.com>
2018-02-22 18:52:16 +0800

    update code

--- glua_bridge.go
+++ glua_bridge.go
@@ -9,6 +9,32 @@ import (
 //#include "glua.h"
 import "C"
 
+func LuaNumberToInt64(value interface{}) (int64, error) {
+	switch value.(type) {
+	case C.lua_Number:
+		{
+			return int64(value.(C.lua_Number)), nil
+		}
+	default:
+		{
+			return 0, errors.New("Invalid Type")
+		}
+	}
+}
+
+func LuaNumberToInt32(value interface{}) (int32, error) {
+	switch value.(type) {
+	case C.lua_Number:
+		{
+			return int32(value.(C.lua_Number)), nil
+		}
+	default:
+		{
+			return 0, errors.New("Invalid Type")
+		}
+	}
+}
+
 func LuaNumberToInt(value interface{}) (int, error) {
 	switch value.(type) {
 	case C.lua_Number:
@@ -30,7 +56,20 @@ func LuaNumberToFloat32(value interface{}) (float32, error) {
 		}
 	default:
 		{
-			return 0, errors.New("Invalid Type")
+			return 0.0, errors.New("Invalid Type")
+		}
+	}
+}
+
+func LuaNumberToFloat64(value interface{}) (float64, error) {
+	switch value.(type) {
+	case C.lua_Number:
+		{
+			return float64(value.(C.lua_Number)), nil
+		}
+	default:
+		{
+			return 0.0, errors.New("Invalid Type")
 		}
 	}
 }
